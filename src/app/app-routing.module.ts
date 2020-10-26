8c5cfa790ec830250a930ac642a03b535f4d70e2
Pushkar JK <pushkarjk@gmail.com>
2020-10-25 20:48:45 -0500

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ProfileComponent } from './profile/profile.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+import { RegisterComponent } from './register/register.component';
+import { ForumComponent } from './forum/forum.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    pathMatch: 'full',
+    component: LoginComponent
+  },
+  {
+    path: 'Adminlogin',
+    pathMatch: 'full',
+    component: AdminLoginComponent
+  },
+  {
+    path: 'inventory',
+    pathMatch: 'full',
+    component: InventoryComponent
+  },
+  {
+    path: 'profile',
+    pathMatch: 'full',
+    component: ProfileComponent
+  },
+  {
+    path: 'questionnaire',
+    pathMatch: 'full',
+    component: QuestionnaireComponent
+  },
+  {
+    path: 'register',
+    pathMatch: 'full',
+    component: RegisterComponent
+  },
+  {
+    path: 'forum',
+    pathMatch: 'full',
+    component: ForumComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+import { ForumComponent } from './forum/forum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminLoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    InventoryComponent,
+    QuestionnaireComponent,
+    ForumComponent
   ],
   imports: [
     BrowserModule,
